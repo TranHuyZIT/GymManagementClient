578c484515d07a95034bdff602c381cdbb3973a1
TranHuy2k2 <giahuytran2002@gmail.com>
2023-02-14 21:33:31 +0700

    initialize project

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:frontend/pages/MyHomepage.dart';
+
+void main() {
+  runApp(DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MyApp(), // Wrap your app
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: "/login",
+      routes: {
+        "/login": (context) => const MyHomePage(title: "Gym Management"),
+        "/home": (context) => const MyHomePage(title: "Gym Management")
+      },
+    );
+  }
+}
